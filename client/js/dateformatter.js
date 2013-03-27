76336229cef592801fc6a75dafec775cff209bcc
uchida <uchida.atsushi.8@gmail.com>
2013-03-27 19:30:40 +0900

    change dateformatter

--- client/js/dateformatter.js
+++ client/js/dateformatter.js
@@ -1,17 +1,36 @@
 var DateFormatter = function(time){
-	var unixTime = time;
+	var deltaTime = new Date().getTime() - time;
+	var minute = 60000;
+	var hour   = 3600000;
+	var day    = 86400000;
+	var month  = 2592000000;
+	var year   = 31104000000;
 
-	function fixString(i) {
-		return (i<10)?"0"+i:""+i;
-	}
-
-	this.format = function(){
-		var t = new Date(unixTime);
-		var fMonth   = fixString(t.getMonth()+1);
-		var fDate    = fixString(t.getDate());
-		var fHours   = fixString(t.getHours());
-		var fMinutes = fixString(t.getMinutes());
-		var fSeconds = fixString(t.getSeconds());
-		return t.getFullYear() + "/" + fMonth + "/" + fDate + " " + fHours + ":" + fMinutes + ":" + fSeconds;
+	this.format = function() {
+		if(deltaTime < minute) {
+			return "just now";
+		}else if(deltaTime >= minute && deltaTime < 2 * minute) {
+			return "a minute ago";
+		}else if(deltaTime >= 2 * minute && deltaTime < hour) {
+			return "" + Math.floor(deltaTime / minute) + " minutes ago";
+		}else if(deltaTime >= hour && deltaTime < 2 * hour) {
+			return "an hour ago";
+		}else if(deltaTime >= 2*hour && deltaTime < day) {
+			return "" + Math.floor(deltaTime / hour) + " hours ago";
+		}else if(deltaTime >= day && deltaTime < 2 * day) {
+			return "a day ago";
+		}else if(deltaTime >= 2*day && deltaTime < month) {
+			return "" + Math.floor(deltaTime / day) + " days ago";
+		}else if(deltaTime >= month && deltaTime < 2 * month) {
+			return "a month ago";
+		}else if(deltaTime >= 2*month && deltaTime < year) {
+			return "" + Math.floor(deltaTime / day) + " months ago";
+		}else if(deltaTime >= year && deltaTime < 2 * year) {
+			return "an year ago";
+		}else if(deltaTime >= 2*year) {
+			return "" + Math.floor(deltaTime / year) + " years ago";
+		}else {
+			return "error";
+		}
 	}
 };
