0dae791ba84973c02571107df74c6af55762f514
uchida <uchida.atsushi.8@gmail.com>
2013-03-28 15:31:59 +0900

    fix typo

--- client/index.php
+++ client/index.php
@@ -11,7 +11,7 @@ if(isset($_COOKIE["userId"])) {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>AssuaranceScriptEditor</title>
+<title>AssuranceScriptEditor</title>
 <link rel="stylesheet" type="text/css" href="lib/jquery.svg.css"/>
 <link rel="stylesheet" type="text/css" href="lib/jquery.ui.autocomplete.css"/>
 <link rel="stylesheet" type="text/css" href="lib/jquery.colorPicker.css"/>
